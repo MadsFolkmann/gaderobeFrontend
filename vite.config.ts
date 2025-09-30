901d452863b3febdffc28d33306d2727ae9d54de
MadsFolkmann <124000700+MadsFolkmann@users.noreply.github.com>
2025-09-30 12:11:34 +0200

    Deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gaderobe_frontend/',
+  base: '/gaderobeFrontend/',
   plugins: [react(),
   tailwindcss()
   ],
